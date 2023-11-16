bad132eb9cc42f6778035eb52a0b9fda59000d69
giovani <giovaniocan@gmail.com>
2023-11-16 17:05:00 -0300

    ♻️:home page now is all responsive

--- src/components/home/ProductCard.tsx
+++ src/components/home/ProductCard.tsx
@@ -7,9 +7,9 @@ import { Minus, Plus, ShoppingCart } from 'phosphor-react'
 
 export function ProductCard() {
   return (
-    <div className="max-w-[16rem] bg-base-card flex flex-col  px-5 py-6  rounded-tl-md rounded-tr-[36px] rounded-br-md rounded-bl-[36px] ">
+    <div className="max-w-[16rem] bg-base-card flex flex-col px-5 py-6 rounded-tl-md rounded-tr-[36px] rounded-br-md rounded-bl-[36px] ">
       <div className="flex flex-col items-center">
-        <div className="relative  w-36 h-40">
+        <div className="relative w-24 h-28 md:w-36 md:h-40">
           <Image
             src={image}
             alt="Pão de Queijo"
@@ -18,28 +18,28 @@ export function ProductCard() {
             className="object-cover rounded-lg"
           />
         </div>
-        <div className="mt-3 flex justify-around">
+        <div className="hidden mt-3 md:flex w-full flex-wrap justify-around">
           <Tags>teste</Tags>
           <Tags>sla</Tags>
           <Tags>sla</Tags>
         </div>
         <h2
-          className={`${baloo700.className}  text-base-subtitle text-xl mt-4 `}
+          className={`${baloo700.className} text-center text-base-subtitle text-lg md:text-xl mt-4`}
         >
           nome do produto
         </h2>
-        <span className="text-sm text-base-label text-center ">
-          Explosão de sabores irresistiveis e especiais
+        <span className="text-xs md:text-sm mt-2 text-base-label text-center">
+          Explosão de sabores irresistíveis e especiais
         </span>
       </div>
 
-      <div className="flex gap-6 mt-8">
+      <div className="flex flex-col items-center mt-3 gap-2 md:flex-row md:gap-4 md:mt-8">
         <div className="flex items-center gap-[2px]">
           <span className="text-sm">R$</span>
           <h3 className={`${baloo700.className} text-2xl`}>9,90</h3>
         </div>
-        <div className="flex gap-2">
-          <div className="flex gap-1 bg-base-button items-center py-2 px-3 rounded-lg">
+        <div className="flex gap-2 md:gap-1 ">
+          <div className="flex gap-2 bg-base-button items-center py-2 px-3 rounded-lg">
             <Minus color="#8047F8" size={22} />
             <p className="text-base text-base-title">1</p>
             <Plus color="#8047F8" size={22} />
--- src/components/home/ProductsList.tsx
+++ src/components/home/ProductsList.tsx
@@ -3,11 +3,12 @@ import { ProductCard } from './ProductCard'
 
 export function ProductsList() {
   return (
-    <div className="flex flex-col gap-14">
+    <div className="flex mx-6 pb-6 md:mx-0 flex-col gap-14">
       <h2 className={`${baloo800.className} text-4xl text-base-subtitle`}>
         Nossos produtos
       </h2>
-      <main className="flex gap-20 flex-wrap  ">
+      <main className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6">
+        {/* Adjust the number of ProductCard instances based on your actual data */}
         <ProductCard />
         <ProductCard />
         <ProductCard />
