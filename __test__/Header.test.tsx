a9a502a5279542bb26e078fb287f32e172df96bc
giovani <giovaniocan@gmail.com>
2023-12-02 16:40:21 -0300

    install tests packages and added an exemplo test

---
+++ __test__/Header.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "@/redux/store";
+import Home from "@/pages/home";
+
+describe("Teste do Header", () => {
+  it("Deve renderizar o componente Header", () => {
+    render(
+      <Provider store={store}>
+        <Home />
+      </Provider>
+    );
+
+    const textoEsperado = "Pão de queijo";
+    const teste = screen.getByText(textoEsperado);
+
+    expect(teste).toBeInTheDocument();
+  });
+});
