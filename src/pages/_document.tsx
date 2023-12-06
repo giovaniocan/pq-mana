9f8c88a59bba4f99df96ead7d8d671af66d56563
giovani <giovaniocan@gmail.com>
2023-12-06 14:15:45 -0300

    ✅:added mana-logo in the page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link rel="shortcut icon" href="/logo.svg" type="image/x-icon" />
       <body>
         <Main />
         <NextScript />
